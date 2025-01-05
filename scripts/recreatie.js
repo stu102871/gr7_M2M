5090c8cb84365ee7fd68901c3bd170b905da0871
stu102871 <102871@glr.nl>
2025-01-05 06:11:15 +0000

    Tayrin: Recreatiepagina Setup

---
+++ scripts/recreatie.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+
+// Variable Prep
+
+const output = {
+    img  : document.getElementById("gameImg"),
+    title: document.getElementById("gameTitle"),
+    info : document.getElementById("gameInfo"),
+    index: document.getElementById("index")
+}

1962569a904047a6aae481bcc37d39ce3cbbbac1
Salahedin90 <102454@glr.nl>
2025-01-15 14:50:37 +0000

    gewerkt aan e book pagina css bijgewerkt

--- scripts/series.js
+++ scripts/series.js
@@ -1,14 +1,5 @@
-const musicTracks = {
-    "Boef": "../../media/music/Littekens.mp3",
-    "Lady gaga": "../../media/music/Lady Gaga - Bloody Mary (Official Audio).mp3",
-    "Katy Eerry": "../../media/music/Katy Perry - I Kissed A Girl (Official Music Video).mp3",
-    "Billie Eilish": "../../media/music/Billie Eilish, Khalid - lovely.mp3",
-    "Kendrick Lamar": "../../media/music/Kendrick Lamar - Not Like Us.mp3",
-    
-    "Beyonce": "../../media/music/Beyoncé - Halo.mp3",
-    "Rihanna": "../../media/music/Rihanna - Umbrella (Orange Version) (Official Music Video) ft. JAY-Z.mp3",
-    "The Weekend": "../../media/music/The Weeknd - Blinding Lights (Official Video).mp3",
-    "Ariana Grande": "../../media/music/Ariana Grande - 7 rings (Official Video).mp3"
+const seriesTrailers = {
+
 };
 
 const gameImgs = document.querySelectorAll('.photoBox img');
